7c34ad19e24267fe512d76a35626e4d3dc73ef77
jeenunim <jeenunim@DESKTOP-DO77UPR>
2023-07-06 14:45:14 -0700

    about halfway through completing the DBMS set up with migration and seed files

---
+++ migrations/20230706183244_weapon_system_list.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('weapon_system', table => {
+    table.increments('id');
+    table.string('name');
+    table.integer('weapon_type_id');
+    table.foreign('weapon_type_id').references('weapon_type.id');
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  //we will rollback table and foreign keys
+  return knex.schema.alterTable('weapon_system', table => {
+    table.dropForeign('weapon_type_id')
+    }) //then we drop table itself
+    .then(function () {
+      return knex.schema.dropTableIfExists('weapon_system');
+    });
+};
