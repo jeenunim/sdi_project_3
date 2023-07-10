9dcf9ef3ecc84ae0550cea08856955524194d8b5
Miguel <angel.miguelr@yahoo.com>
2023-07-10 09:53:42 -0700

    working on threat page

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,7 +1,7 @@
 import styles from './App.css';
 import React, { createContext } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Header from '../Navigation/Header'
+import Header from '../Navigation/Header';
 import Home from '../Home/home'
 
 function App() {
@@ -9,49 +9,15 @@ function App() {
 
 
   return (
-      <div className="App">
-        <div>
-
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/targetDetails' element={<>you are in target details</>} />
-            <Route path='/addThreatPage' element={<>you are in threat details</>} />
-            <Route path='/weaponDetailsPage' element={<>you are in weapon details</>} />
-          </Routes>
-        </div>
-        <div>
-
-          <header>
-          <Header />
-            <h1>Our App</h1>
-          </header>
-
-                          <main>
-                            <section className="cards">
-                              <div className="card">
-                                <img src="card1.jpg" alt="Card 1" />
-                                <h2>Card 1</h2>
-                                <p>Threats</p>
-                                <Link to="/weaponDetailsPage">Read More</Link>
-                              </div>
-
-                              <div className="card">
-                                <img src="card2.jpg" alt="Card 2" />
-                                <h2>Card 2</h2>
-                                <p>Weapons</p>
-                                <Link to="/addThreatPage">Read More</Link>
-                              </div>
-
-                              <div className="card">
-                                <img src="card3.jpg" alt="Card 3" />
-                                <h2>Card 3</h2>
-                                <p>Details</p>
-                                <Link to="/targetDetailsPage">Read More</Link>
-                              </div>
-                            </section>
-                          </main>
-                          </div>
-      </div>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/targetDetails' element={<>you are in target details</>} />
+        <Route path='/addThreatPage' element={<>you are in threat details</>} />
+        <Route path='/weaponDetailsPage' element={<>you are in weapon details</>} />
+      </Routes>
+    </div>
   );
 }
 
--- frontend/src/Home/home.js
+++ frontend/src/Home/home.js
@@ -1,10 +1,71 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom'
 
-
+const [threatCard, setThreatCard] = useState([]);
 
 const Home = () => {
 
+// export const threatCardContext= React.createContext();
+// what is the onclick giving back -> provide feedback to backend
+// is it not giving back entire object?
+// what makes sense for an endpoint
+
+//set threat card on home page and console log that out
+
+/*
+useEffect(() => {
+
+})
+ */
+
+
+
+// // handle on click
+// const handleOnClick = (selectedCard) => {
+//   //on click we want to setThreatCard to the card that we clicked
+//   //when we click we want all of the information to populate the threat page
+//   //information we need:
+//   //img, target details then we also want what is good against that target
+
+//   //we setThreatCard information here?
+//   setThreatCard([selectedCard]) //when we setThreatCard
+// }
+
   return (
-    <>Your are home, this is where you html is going to go</>
+    <main>
+      <h1>Home Page</h1>
+      <section className="cards-container">
+        <div className="card">
+          <h2>Tank T-54</h2>
+          <p>Threats</p>
+          <Link to="/targetDetails"><img id="t54" src="https://wiki.wgcdn.co/images/4/4d/AnnoR40_T-54.png" alt="Card 1" /></Link>
+        {/* onclick */}
+
+        </div>
+
+
+
+
+{/*
+        <div className="card">
+
+          <h2>Card 2</h2>
+          <p>Weapons</p>
+          <Link to="/targetDetails"> <img src="https://wiki.wgcdn.co/images/4/4d/AnnoR40_T-54.png" alt="Card 2" /></Link>
+        </div>
+
+        <div className="card">
+
+          <h2>Card 3</h2>
+          <p>Details</p>
+          <Link to="/targetDetails"> <img src="https://miniart-models.com/wp-content/uploads/2016/07/0431.jpg" alt="Card 3" /></Link>
+        </div> */}
+
+      </section>
+      {/* <threatCardContext.Provider value={ {target} }>
+          </>
+      </threatCardContext.Provider> */}
+    </main>
   );
 }
 
